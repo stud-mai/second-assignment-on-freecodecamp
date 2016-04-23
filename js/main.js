aa485411045e169d21354ebe19933972f7b86b14
Evgeny <e.a.zhuchkov@gmail.com>
2016-04-24 01:00:30 +0300

    Update main.js
    Added a new function for scrolling thumbnail picture to bottom to see all the picture

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function(){
+	//Scrolls to clicked section
 	$('.navbar li a').on('click', function(e) {
 		event.preventDefault();
-		
 		var link = $(this).attr('href');
 		
 		if (window.innerWidth > 767) {
@@ -9,6 +9,13 @@
 		} else {
 			$('body, html').animate({scrollTop:($(link).offset().top - 50)}, 500);
 		}
-			
+	});
+	
+	//Scrolls thumbnail pic to bottom
+	$('.work_thumbnail a').hover(function() {
+		var el = $(this).find('img');
+		el.css('top', 150 - el.height());
+	}, function() {
+		$(this).find('img').css('top', 0);
 	});
 })();
